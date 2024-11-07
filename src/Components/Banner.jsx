97d22baadd1cd4bfe6595d4a6e3965b108767c81
shine2758 <shinecs1003@gmail.com>
2024-11-07 15:26:12 +0530

    banner

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      {/* <!--------Left Side-------------!> */}
+      <div>
+
+      </div>
+      
+      {/* <!--------right Side-------------!> */}
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../Components/Header'
-import SpecialityMenu from '../Contacts/SpecialityMenu'
+import SpecialityMenu from '../Components/SpecialityMenu'
+import TopDoctors from '../Components/TopDoctors'
+import Banner from '../Components/Banner'
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu/>
+      <TopDoctors/>
+      <Banner/>
     </div>
   )
 }
