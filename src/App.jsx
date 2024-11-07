97f7792240ccb3f54c4d687fc25408a0712a5a96
shine2758 <shinecs1003@gmail.com>
2024-11-07 11:30:33 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import Header from '../Components/Navbar'
-import Header from '../Components/Header'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Doctors from './Pages/Doctors';
+import Login from './Pages/Login';
+import About from './Pages/About';
+import Contact from './Pages/Contacts';
+import MyProfile from './Pages/MyProfile';
+import MyAppointements from './Pages/MyAppointments';
+import Appointments from './Pages/Appointments';
+import Navbar from './Components/Navbar';
+
 const App = () => {
   return (
-    <div>
-     <Navbar/>
-     <Header/>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointements />} />
+        <Route path='/appointments' element={<Appointments />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
