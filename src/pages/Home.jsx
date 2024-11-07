1c50c92ae974aa930d2c0b3a22d9b578c6375c34
shine2758 <shinecs1003@gmail.com>
2024-11-07 13:05:44 +0530

    add docters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../Components/Header'
+import SpecialityMenu from '../Contacts/SpecialityMenu'
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialityMenu/>
     </div>
   )
 }
